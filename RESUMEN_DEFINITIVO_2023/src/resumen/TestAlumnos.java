a7bde6fe317c5c5c5eadc2f8b83d7fbc39ccb2f4
Victoria Denisse Reynoso <victoriareynoso88@gmail.com>
2023-05-22 23:44:13 -0300

    rrrrr

--- RESUMEN_DEFINITIVO_2023/src/resumen/TestAlumnos.java
+++ RESUMEN_DEFINITIVO_2023/src/resumen/TestAlumnos.java
@@ -15,7 +15,7 @@ public class TestAlumnos {
 		boolean tieneDudas;
 		int nota1,nota2;
 		
-		//holavadyrr
+		//holavadyrrr
 		
 		//Pediremos datos de un alumno para luego crear el objeto que los guarde:
 		
